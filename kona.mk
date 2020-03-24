9838c0bfb1c5400c6436f4f0b57f964e422d1bf3
trpsl <trpsl755@gmail.com>
2020-03-24 14:10:26 +0100

    sm8250-common: Add Moto Dolby
    Values taken from OP9 Pro and modified by Sasha (@helenius147 on Telegram)
    to work with Moto Dolby.
    
    Co-authored-by: blankaf <realblankaf@gmail.com>
    Co-authored-by: MB <mb150996@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Change-Id: I5450fed2cb2add4117aaba6a57aafaadc9ba77fb
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- kona.mk
+++ kona.mk
@@ -196,6 +196,12 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     libadpcmdec
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.dolby.ds2.enabled=true \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    persist.vendor.audio_fx.current=dolby \
+    ro.vendor.dolby.dax.version=DS1_2.2.0.0_r1
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
@@ -208,7 +214,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    device/xiaomi/sm8250-common/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Audio-Legacy for Muting logspam
 PRODUCT_PACKAGES += \
