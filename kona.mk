1b5a372adc34a8632b10dfa13fd6895dc7d09b4c
johnmart19 <johnivan19999@gmail.com>
2022-04-19 00:10:46 +0200

    sm8250-common: Declare FCM and target for usb@1.3
    android.hardware.usb@1.3::IUsb/default
    
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- kona.mk
+++ kona.mk
@@ -684,8 +684,9 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3 \
     android.hardware.usb@1.3.vendor \
-    android.hardware.usb@1.2-service-qti
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
