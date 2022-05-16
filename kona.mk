94ecc3a12e1048f64ad60ec26555323d17eae4c7
Johnmart19 <>
2022-05-16 21:28:45 +0530

    sm8250-common:update blob from miui 13.0.4.0 europe

--- kona.mk
+++ kona.mk
@@ -419,8 +419,11 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0 \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.1.vendor
-
+   
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
