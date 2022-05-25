e11e68723a90a73f1f6855146f6b708ed026eab3
revolwoc <imsakshisharma04@gmail.com>
2022-05-25 14:13:40 +0530

    Revert "sm8250-common: Remove MusicFX"
    This reverts commit d744f0313ab1f9454159e0a6b267505695b631ff.

--- kona.mk
+++ kona.mk
@@ -626,10 +626,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
-    
-# RemovePackages
-PRODUCT_PACKAGES += \
-    RemovePackages
 
 # RCS
 PRODUCT_PACKAGES += \
