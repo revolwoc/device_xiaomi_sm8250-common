98b37310a64dcf6c036a332d639b4af9f5554809
darkphnx <dark.phnx12@gmail.com>
2022-09-08 06:26:11 +0000

    [squash]: sm8250-common: Drop Dirac
    Revert "sm8250-common: parts: Change Headphone types for dirac"
    
    This reverts commit 58db153facdfde57bf2c25e2b4eba2cb27790d6f.
    
    Revert "sm8250-common: parts: dirac: Add hifi vector icons"
    
    This reverts commit 3d4a14ee412ec6a21d9430ffcb47d13f6b1c72e8.
    
    Revert "sm8250-common: parts: dirac: Update vector icons to MD2 versions"
    
    This reverts commit 8c2dd4319cbb917e0e9904ee38301e846ed4fd98.
    
    Revert "sm8250-common: parts: dirac: Add vector icons"
    
    This reverts commit fb4dba2dec967c621e7ee7e6cab640109796d658.
    
    Revert "sm8250-common: parts: Update and polish vector drawables"
    
    This reverts commit 1159ca304f4827962d03a79a6ce109125f371e46.
    
    Revert "sm8250-common: parts: Add more Dirac presets"
    
    This reverts commit fda156b9805a5f845996d5f1672ee8a4cb739344.
    
    Revert "sm8250-common: parts: Add MiSound scenes"
    
    This reverts commit 428389109e801a328bc23e1f1a242db343fb1f5a.
    
    Revert "sm8250-common: parts: Enable hi-fi mode regardless headset and preset"
    
    This reverts commit 158aaa44898382e9cd3503f24d69b5e0e3e236ff.
    
    Revert "sm8250-common: parts: Use TOGGLEABLE_TILE in Dirac"
    
    This reverts commit 9108b90bec70ce9fc6114a6ea6b0d8c35283ed67.
    
    Revert "sm8250-common: parts: Refactor dirac code"
    
    This reverts commit 807c3379d44ec67d27947d855f604ecde3c5e0af.
    
    Revert "sm8250-common: parts: Handle more error for dirac"
    
    This reverts commit 3aa6ad61fdae6c7eacc60f2b04bde4b4df3566a7.
    
    Revert "sm8250-common: parts: Add bass booster preset for MiSound"
    
    This reverts commit b3a697c19b89dc263a4a3894d9646f34d60fa5d9.
    
    Revert "sm8250-common: parts: Dirac fixes"
    
    This reverts commit b76c62be7faa6d487c73547c0f8d7757c57304de.
    
    Revert "sm8250-common: parts: Disable hifi preference when toggling switchbar"
    
    This reverts commit 9792151168371ca85df642dfa166753aa843e965.
    
    Revert "sm8250-common: parts: Dirac: Adapt to S style"
    
    This reverts commit 0f6d88989dfcb526c7b1c5e673f83e972f8f912f.
    
    Revert "sm8250-common: parts: Add summary for Dirac"
    
    This reverts commit b1f8fd8e007814ac51a32eaa2250da9cc76ac58a.
    
    Revert "sm8250-common: parts: Prevent NPE before DiracSound is initialized"
    
    This reverts commit d747f7242a003a8fedef78f59e76047c5ba01d58.
    
    Revert "sm8250-common: Dont restore values on boot"
    
    This reverts commit 72117832100a5c3e4b2e23e972feedc2d1956ef2.
    
    Revert "sm8250-common: Add support for hifi"
    
    This reverts commit 120d3ac85389fda1a10187fdf43cf0c2616feaf3.
    
    Revert "sm8250-common: parts: Add DiracSound backend"
    
    This reverts commit 02087a467ffd087f750bc5c2882597713ad0eec8.
    
    Revert "sm8250-common: parts: Introduce Dirac preferences"
    
    This reverts commit e07279205ebfafae661f764f29a5f993127232ce.
    
    Revert "sm8250-common: parts: Use directBootAware"
    
    This reverts commit ff8e56361f5c069dc944640aff8c43d6e2780e74.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -36,16 +35,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        try {
-            DiracUtils.getInstance(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
-        RefreshUtils.startService(context);
+        RefreshUtils.startService(context);        
     }
 }
