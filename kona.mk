327a7e05dfc13ad3bea64928138c025f7e1831f0
johnmart19 <johnivan19999@gmail.com>
2022-09-29 17:33:13 +0300

    sm8250-common: Guard Virtual A/B XOR Flag
    Signed-off-by: ∂α૨ҡ_ρɦσεɳเא <dark.phnx12@gmail.com>

--- kona.mk
+++ kona.mk
@@ -10,6 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+ifeq ($(TARGET_IS_VAB),true)
+# Enable compression with xor
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
+endif
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
