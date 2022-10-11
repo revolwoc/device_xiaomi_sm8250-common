ef3336a4f5fb7786f7736805e6033780ffdaef7f
johnmart19 <johnivan19999@gmail.com>
2022-10-11 11:37:33 +0300

    sm8250-common: Dolby: Move Permissions to Device trees
    - Needed to be able to freely modify and update them if required

--- kona.mk
+++ kona.mk
@@ -294,6 +294,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Dolby Permissions
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/permissions/,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
