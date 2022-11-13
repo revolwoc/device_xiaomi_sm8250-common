9b941455771cebed84b1d3115c6b0452ea679e14
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-13 20:10:47 +0100

    sm8250-common: Remove non-existing packages
    Change-Id: Ia154904be35b6d6b65f5879be244d2168354bd7e

--- kona.mk
+++ kona.mk
@@ -306,7 +306,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
