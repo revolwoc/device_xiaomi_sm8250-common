3a8f5acbf2f346667981cc4763d4bca53e72f82b
∂α૨ҡ_ρɦσεɳเא <dark.phnx12@gmail.com>
2022-11-14 08:08:55 +0530

    sm8250:common: Revert Erofs changes
    Revert "sm8250-common: Set pcluster size per aosp"
    
    This reverts commit 9188d7d18274cd212042818906af0c851b2884d7.
    
    Revert "sm8250-common: Use lz4 compression for erofs"
    
    This reverts commit 203494fadc50e4b6e1835810c51e8828d724770b.
    
    Revert "sm8250-common: Swich to EROFS"
    
    This reverts commit d6383874b112d2dce5a65ab41e98eb276e5c9807.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,13 +150,8 @@ TREBLE_PARTITIONS := odm vendor
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs) \
+    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
-
-# EROFS
-BOARD_EROFS_COMPRESSOR := lz4
-BOARD_EROFS_PCLUSTER_SIZE := 262144
-PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
 # Partitions - dynamic
 BOARD_SUPER_PARTITION_SIZE := 9126805504
